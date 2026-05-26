770370be699ec37a53c6ef97e9540c01dca798cc
Krishna <pgpkrish003@gmail.com>
2026-05-26 16:07:02 +0530

    commit

---
+++ Data Anotation/app.py
@@ -0,0 +1,26 @@
+import cv2
+import os 
+
+output_folder = "frames"
+video_path = r"d:\Mlops_Training\Data Anotation\videos\data.mp4"
+
+cap = cv2.VideoCapture(video_path)
+os.makedirs(output_folder, exist_ok=True)
+
+frame_count = 0
+count = 0
+while True:
+    success, frame = cap.read()
+    if not success:
+        break
+    if frame_count%5 ==0:
+        file = os.path.join(
+            output_folder,
+            f"frame_{count}.jpg"
+        )
+        cv2.imwrite(file,frame)
+
+        count+=1
+    frame_count+=1
+cap.release()
+print("Frames extracted")
---
+++ Video_to_frame/app.py
@@ -0,0 +1,23 @@
+import os
+import cv2
+os.chdir(r"D:\Mlops_Training\Video_to_frame")
+cap = cv2.VideoCapture(r"D:\Mlops_Training\Video_to_frame\WIN_20260523_09_15_33_Pro.mp4")
+framerate = int(cap.get(cv2.CAP_PROP_FPS))
+interval = max(1, round(framerate * 0.25))
+framecount = 0
+count = 0
+while True:
+    success, frame = cap.read()
+    if not success:
+        break
+    framecount += 1
+    if framecount >= interval:
+        print(f"Saving frame: {count}")
+        frame = cv2.resize(frame,(1280,720))
+        cv2.imwrite(f"11_frames{count}.jpg",frame)
+        count+=1
+        framecount = 0
+
+cap.release()
+cv2.destroyAllWindows()
+print("completed")
